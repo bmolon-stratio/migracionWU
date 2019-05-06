6879e96cdda971de65341175d50b2e296e7a20c0
bmolon <bmolon@stratio.com>
2019-05-06 17:47:20 +0200

    .

---
+++ src/migracionWU.java
@@ -0,0 +1,22 @@
+public class migracionWU {
+    public static void main (String [ ] args) {
+        //Consultar medidas
+
+
+        //Traemos id_dispositivo, valor, id_magnitud, fecha y timezone
+
+
+        //Consultar dispositivos y traer el payload
+
+
+        //Obtener latitud y longitud del payload de dispositivos
+
+
+        //Consultar en activos la aproximación de latitud y longitud
+
+
+        //Guardar los insert en fichero
+
+
+    }
+}
